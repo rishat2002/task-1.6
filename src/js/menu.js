700e596d9ddb367636d3b61afb747a17d5938d93
rishat2002 <gabdulzhaparov.rischat@yandex.ru>
2020-10-27 09:07:17 +0300

    # change

--- src/js/menu.js
+++ src/js/menu.js
@@ -2,20 +2,20 @@ const menu = document.querySelector('.menu');
 const containerBlock = document.querySelector('.container');
 const closeMenuBtn = document.querySelector('.menu__close-btn');
 const openMenuBtn =document.querySelector('.header__button-burger');
-const closeOverlay=document.querySelector('.overlay--menu');
+const overlay=document.querySelector('.overlay--menu');
 const modal = document.querySelectorAll('.modal');
 
 function openMenu () {
        menu.classList.add('menu--active');
-       closeOverlay.addEventListener('click',closeOverlayClickHandler);
-       closeOverlay.classList.add('overlay--active');
+       overlay.addEventListener('click',closeOverlayClickHandler);
+       overlay.classList.add('overlay--active');
        closeMenuBtn.addEventListener('click',closeMenuBtnClickHandler);
        document.addEventListener('keydown',escapeKeydownMenuHandler);
 }
 function closeMenu() {
        menu.classList.remove('menu--active');
-       closeOverlay.classList.remove('overlay--active');
-       closeOverlay.removeEventListener('click',closeOverlayClickHandler);
+       overlay.classList.remove('overlay--active');
+       overlay.removeEventListener('click',closeOverlayClickHandler);
        closeMenuBtn.removeEventListener('click',closeMenuBtnClickHandler);
        document.removeEventListener('keydown', escapeKeydownMenuHandler
 ) }
